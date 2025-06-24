4fd4d2f08f54842bf25633027f13604a362b60b7
ABHIJEET <abhijeet.kumar@avaloninfosys.com>
2025-06-24 16:46:58 +0530

    login and logout page integrated with backend

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,47 +1,52 @@
 import React, { useState } from "react";
-import { View, TextInput, Button, Text, StyleSheet, Alert } from "react-native";
-import AppLayout from "../layouts/AppLayout";
+import { View, TextInput, Button, Text, StyleSheet } from "react-native";
+import axios from 'axios'
 
 export default function LoginScreen({ navigation }: any) {
-  
-  const [email, setEmail] = useState("");
+
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    if (email === "test@example.com" && password === "123456") {
-      navigation.navigate("Home");
-    } else {
-      Alert.alert("Invalid credentials");
+  const handleLogin = async () => {
+    try {
+      const payload = {
+        username: username,
+        password: password
+      }
+      const response = await axios.post('http://192.168.86.71:8000/auth', payload)
+      const token = response.data.token
+      navigation.navigate("Home")
+      console.log("object");
+    } catch (error) {
+      console.log(error);
     }
   };
 
   return (
-    <AppLayout navigation={navigation}>
-      <View style={styles.container}>
-        <Text style={styles.heading}>Login</Text>
-        <TextInput
-          placeholder="Email"
-          style={styles.input}
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-        <TextInput
-          placeholder="Password"
-          style={styles.input}
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
-        <Button title="Login" onPress={handleLogin} />
-      </View>
-    </AppLayout>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Login</Text>
+      <TextInput
+        placeholder="Email"
+        style={styles.input}
+        value={username}
+        onChangeText={setUsername}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder="Password"
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", padding: 20,  },
+  container: { flex: 1, justifyContent: "center", padding: 20, },
   heading: { fontSize: 30, marginBottom: 30, textAlign: "center" },
   input: { borderWidth: 1, padding: 10, marginBottom: 15, borderRadius: 5 },
 });
